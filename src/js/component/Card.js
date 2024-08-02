ba62d25c74cb48a91fd82d48b5ef3ad9a21e85ef
Sebastian Solano <32124063+sbs29@users.noreply.github.com>
2024-08-02 16:53:49 +0000

    Pull without description

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -4,21 +4,18 @@ import { Context } from "../store/appContext";
 
 export const Card = props => {
 
-    //console.log(props.item);
     const { store, actions } = useContext(Context);
 
     function OnImageError(e) {
         e.target.src = "https://www.tictacsoluciones.com/wp-content/uploads/2012/07/star-wars.png"
     }
 
-    //console.log(props.item);
-
     return (
         <div className="card" style={{ width: "22rem" }}>
             <img onError={OnImageError} src={props.item.imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{props.item.name}</h5>
-                <p>{props.item.description}</p>
+                <p>{store.infoDetails.gender}</p>
                 <Link to={`${props.type}/${props.item.uid}`}>
                     <button className="btn btn-outline-info">Learn more!</button>
                 </Link>
